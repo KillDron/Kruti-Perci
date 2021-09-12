9e51554a937b5b80c7010cc799a7b7007ae6c843
SophiaOrshulyak <90265478+SophiaOrshulyak@users.noreply.github.com>
2021-09-12 22:43:33 +0300

    Update question7.py

--- question7.py
+++ question7.py
@@ -1,5 +1,4 @@
-Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:34:40) [MSC v.1927 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
+
 a = (int(input('Price of the meal:')))
 b = (int(input('Percentage on tip:')))
 print('Tips:', (b*(a/100)))
