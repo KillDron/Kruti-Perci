dc91792ea05c3833fd83d631af948ef1041822dc
KillDron <90138629+KillDron@users.noreply.github.com>
2021-09-12 23:54:09 +0300

    Update Питання 1.py
    Хз чи так, але най буде

--- Питання 1.py
+++ Питання 1.py
@@ -4,3 +4,10 @@ c = (553/312)**2
 print(a)
 print(b)
 print(c)
+
+ai = int(355/113) * (1 - int(0.003/3522))
+bi = int(22/17) + int(37/47) + int(88/83)
+ci = int(553/312)**2
+print(ai)
+print(bi)
+print(ci)
