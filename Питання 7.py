2210c23ccaac3304f47cdf2bf585eb35c9dff4dd
SophiaOrshulyak <90265478+SophiaOrshulyak@users.noreply.github.com>
2021-09-20 10:52:28 +0300

    Update Питання 7.py

--- Питання 7.py
+++ Питання 7.py
@@ -1,4 +1,4 @@
-a = (int(input('Price of the meal:')))
-b = (int(input('Percentage on tip:')))
+a = (eval(input('Price of the meal:')))
+b = (eval(input('Percentage on tip:')))
 print('Tips:', (b*(a/100)))
 print('Total:', a+(b*(a/100)))
